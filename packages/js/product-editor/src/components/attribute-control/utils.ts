a48ae48898eba6830c448ece19aa0387dad1e7ae
DevDovantien <JohnDev1998@outlook.com>
2024-06-07 09:46:21 -0700

    update

---
+++ packages/js/product-editor/src/components/attribute-control/utils.ts
@@ -0,0 +1,50 @@
+/**
+ * External dependencies
+ */
+import type { ProductProductAttribute } from '@woocommerce/data';
+
+/**
+ * Returns the attribute key. The key will be the `id` or the `name` when the id is 0.
+ *
+ * @param { ProductProductAttribute } attribute product attribute.
+ * @return string|number
+ */
+export function getAttributeKey(
+	attribute: ProductProductAttribute
+): number | string {
+	return attribute.id !== 0 ? attribute.id : attribute.name;
+}
+
+/**
+ * Get an attribute ID that works universally across global and local attributes.
+ *
+ * @param attribute Product attribute.
+ * @return string
+ */
+export const getAttributeId = ( attribute: ProductProductAttribute ) =>
+	`${ attribute.id }-${ attribute.name }`;
+
+/**
+ * Updates the position of a product attribute from the new items list.
+ *
+ * @param { Object } items              key value pair of list items positions.
+ * @param { Object } attributeKeyValues key value pair of product attributes.
+ */
+export function reorderSortableProductAttributePositions(
+	items: Record< number | string, number >,
+	attributeKeyValues: Record< number | string, ProductProductAttribute >
+): ProductProductAttribute[] {
+	return Object.keys( attributeKeyValues ).map(
+		( attributeKey: number | string ): ProductProductAttribute => {
+			if ( ! isNaN( items[ attributeKey ] ) ) {
+				return {
+					...attributeKeyValues[ attributeKey ],
+					position: items[ attributeKey ],
+				};
+			}
+			return {
+				...attributeKeyValues[ attributeKey ],
+			};
+		}
+	);
+}
